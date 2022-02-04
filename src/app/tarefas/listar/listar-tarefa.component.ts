9961530687da6b78c131227772bd2b00246fe83d
Eduardo Nunes <edununes726@gmail.com>
2022-02-04 13:14:19 -0300

    implementando a remocao de tarefas

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -24,7 +24,7 @@ export class ListarTarefaComponent implements OnInit {
     $event.preventDefault();
     if(confirm('Deseja remover a tarefa "' + tarefa.nome + '"?')){
       this.tarefaService.remover(tarefa.id);
-      this.tarefas = this.tarefaService.listarTodos();
+      this.tarefas = this.listarTodos();
     }
   }
 
